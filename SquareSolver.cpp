9b446bbbb50e913f38df91210c85a3477f6c9c1f
RomanKomissarovI <118919859+RomanKomissarovI@users.noreply.github.com>
2024-08-20 19:05:11 +0300

    struct realization

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -5,72 +5,97 @@
 const int SS_INF_CASE = -1; // Ѕесконечное количество корней
 const double EPS = 10e-5; // погрешность
 
+struct coeffs {
+    double a = 0;
+    double b = 0;
+    double c = 0;
+};
 
-double abs(double a){
-    return (a >= 0) ? a : -a;
+enum RootsCount {
+    InfRoots = -1,
+    OneRoots = 1,
+    TwoRoots = 2,
+    ZeroRoots = 0,
+    NOT_SOLVE = -999,
+};
+
+struct solution {
+    double x1 = 0;
+    double x2 = 0;
+    int nroot = NOT_SOLVE;
+};
+
+struct pair {
+    struct coeffs c;
+    struct solution s;
+};
+
+struct pair make_pair() {
+    struct pair p;
+    printf("Square Solver\nEnter the a, b, c:\n");
+    scanf("%lf %lf %lf", &p.c.a, &p.c.b, &p.c.c);
+
+    p.s.x1 = 0;
+    return p;
 }
 
+int solve(struct coeffs c, struct solution* s) {// const struct coeffs* coeffs, struct solution* solution
+    //assert((s->x1) != NULL);
+    //assert(s->x2 != NULL);
+    //assert(s->x1 != s->x2);
 
-int SquareSolver(double a, double b, double c, double* x1, double* x2)
-{
-    assert(x1 != NULL);
-    assert(x2 != NULL);
-    assert(x1 != x2);
-
-    if (abs(a) <= EPS){
-        if (abs(b) <= EPS){
-            return (abs(c) <= EPS) ? SS_INF_CASE : 0;
+    if (fabs(c.a) <= EPS){
+        if (fabs(c.b) <= EPS) {
+            return (fabs(c.c) <= EPS) ? SS_INF_CASE : 0;
         }
-        *x1 = *x2 = -c / b;
+        s->x1 = s->x2 = -c.c / c.b;
         return 1;
     }
 
-    double d = b * b - 4 * a * c;
+    double d = c.b * c.b - 4 * c.a * c.c;
     if (d < 0){
         return 0;
     }
     else{
         double sqr_discr = sqrt(d);
         if (d > EPS){
-            *x1 = (-b - sqr_discr) / 2 / a;
-            *x2 = (-b + sqr_discr) / 2 / a;
+            s->x1 = (-c.b - sqr_discr) / 2 / c.a;
+            s->x2 = (-c.b + sqr_discr) / 2 / c.a;
             return 2;
         }
-        *x1 = *x2 = (-b - sqr_discr) / 2 / a; // d == 0
+        s->x1 = s->x2 = (-c.b - sqr_discr) / 2 / c.a; // d == 0
         return 1;
+    }
+}
+
+void output(struct coeffs c, struct solution* s) {
+    if (s->nroot == NOT_SOLVE) {
+        s->nroot = (RootsCount) solve(c, s);
+    }
+    int root = s->nroot;
+    switch (root){
+    case 0:
+        printf("No roots\n");
+        break;
+    case 1:
+        printf("One root: x = %lg\n", s->x1);
+        break;
+    case 2:
+        printf("Two roots: x1 = %lg;  x2 = %lg\n", s->x1, s->x2);
+        break;
+    case SS_INF_CASE:
+        printf("Infinity number of roots\n");
+        break;
+    default:
+        printf("SS_main() Error");
+        break;
     }
 }
 
 int main()
 {
-    printf("Square Solver\nEnter the a, b, c:\n");
-
-    double a = 0;
-    double b = 0;
-    double c = 0;
-
-    scanf("%lf %lf %lf", &a, &b, &c);
-
-    double x1 = 0;
-    double x2 = 0;
-    int nroot = SquareSolver(a, b, c, &x1, &x2);
-
-    switch (nroot){
-        case 0:
-            printf("No roots\n");
-            break;
-        case 1:
-            printf("One root: x = %lg\n", x1);
-            break;
-        case 2:
-            printf("Two roots: x1 = %lg;  x2 = %lg\n", x1, x2);
-            break;
-        case SS_INF_CASE:
-            printf("Infinity number of roots\n");
-            break;
-        default:
-            printf("SS_main() Error");
-    }
-
+    pair Q;
+    Q = make_pair();
+    output(Q.c, &Q.s);
     return 0;
 }
