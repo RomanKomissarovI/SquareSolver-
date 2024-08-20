9ea14e9bfdfa45ab1d9a2bb0710026a813c5a38b
RomanKomissarovI <118919859+RomanKomissarovI@users.noreply.github.com>
2024-08-20 15:09:30 +0300

    Version 2.0

--- SquareSolver.cpp
+++ SquareSolver.cpp
@@ -1,40 +1,58 @@
 #include <stdio.h>
 #include <math.h>
 
-void qvad(double a, double b, double c)
+const int SS_INF_CASE = -1;
+
+
+int qvad(double a, double b, double c, double* x1, double* x2)
 {
     if (a == 0){
         if (b == 0){
-            if (c == 0){
-                printf("Infinity\n");
-            }
-            else{
-                printf("None\n");
-            }
+            return (c == 0) ? -1: 0;
         }
-        else{
-            printf("%lf\n", (-c) / b);
-        }
+        *x1 = *x2 = -c / b;
+        return 1;
+    }
+
+    double d = b * b - 4 * a * c;
+    if (d > 0){
+        *x1 = (-b - sqrt(d)) / 2 / a;
+        *x2 = (-b + sqrt(d)) / 2 / a;
+        return 2;
+    }
+    else if (d == 0){
+        *x1 = *x2 = (-b - sqrt(d)) / 2 / a;
+        return 1;
     }
     else{
-        double d = b * b - 4 * a * c;
-        if (d > 0){
-            printf("%lf\n", (-b - sqrt(d)) / 2 / a);
-            printf("%lf\n", (-b + sqrt(d)) / 2 / a);
-        }
-        else if (d == 0){
-            printf("%lf\n", (-b - sqrt(d)) / 2 / a);
-        }
-        else{
-            printf("None\n");
-        }
+        return 0;
     }
 }
 
 int main()
 {
+    printf("Square Solver\nEnter the a, b, c:\n");
     double a, b, c;
     scanf("%lf %lf %lf", &a, &b, &c);
-    qvad(a, b, c);
+
+    double x1 = 0, x2 = 0;
+    int nroot = qvad(a, b, c, &x1, &x2);
+
+    switch (nroot){
+        case 0:
+            printf("No roots\n");
+            break;
+        case 1:
+            printf("One root: x = %lg\n", x1);
+            break;
+        case 2:
+            printf("Two roots: x1 = %lg;  x2 = %lg\n", x1, x2);
+            break;
+        case SS_INF_CASE:
+            printf("Infinity number of roots\n");
+            break;
+        default:
+            printf("SS_main() Error");
+    }
     return 0;
 }
